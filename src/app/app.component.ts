d8c96b2bf7ecdce0f810a2011e29ac8152023483
bhavesh1504 <bhaveshnair15@gmail.com>
2022-08-16 18:33:38 +0530

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'number-only';
+  // dev=[
+  //   {'Firstname':'Bhavesh',
+  //     'Lastname':'Nair',
+  //     'Role':'F-E(DEV)'
+  //   },
+  //   {'Firstname':'Vinit',
+  //    'Lastname':'Nair',
+  //    'Role':'B-E(DEV)'
+  //   }
+  // ]
+  // colors=['Red','Yellow','Blue','Green','White','Black']
+}
